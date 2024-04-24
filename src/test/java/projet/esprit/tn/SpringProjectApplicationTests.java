5f8d3352b507bfdca2ec4c08980222cc4c55ee10
samih ouechtati <sameh20950@gmail.com>
2024-04-24 18:01:24 +0100

    spring 1

---
+++ src/test/java/projet/esprit/tn/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.esprit.tn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
